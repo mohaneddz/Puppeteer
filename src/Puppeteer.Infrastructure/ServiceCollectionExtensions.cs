62bc78d508f876912ac3695fd8431a4d34300854
Mohaned <akumaxd5@gmail.com>
2026-09-03 18:22:54 +0100

    chore(infra): wire dependency injection registrations

---
+++ src/Puppeteer.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+using Puppeteer.Core;
+
+namespace Puppeteer.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPuppeteerInfrastructure(this IServiceCollection services, string databasePath) => services
+        .AddSingleton<IProjectDetector, SignatureProjectDetector>()
+        .AddSingleton<IGitMetadataService, GitMetadataService>()
+        .AddSingleton<IProjectScanner, ProjectScanner>()
+        .AddSingleton<IProjectRepository>(_ => new SqliteProjectRepository(databasePath))
+        .AddSingleton<IIconDiscoveryService, IconDiscoveryService>()
+        .AddSingleton<IProjectIconProvider, ProjectIconProvider>()
+        .AddSingleton<IProjectLauncher, ProjectLauncher>()
+        .AddSingleton<ITerminalService, ProcessTerminalService>()
+        .AddSingleton<IFileSystemWatchService, FileSystemWatchService>()
+        .AddSingleton<IProjectClassifier>(_ => new GroqProjectClassifier(new System.Net.Http.HttpClient { Timeout = TimeSpan.FromSeconds(20) }))
+        .AddSingleton<ProjectSearchService>();
+}
